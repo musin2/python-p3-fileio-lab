cb684266c7e0d05bdbd2aa935fd66db86f3bef3c
musin2 <muhsindaud75@gmail.com>
2024-09-13 01:26:25 +0300

    adds lab solution

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    w_file = open(f"{file_name}.txt", mode='w', encoding='utf-8')
+    w_file.write(file_content)
+    w_file.close()
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as a_file:
+        a_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8') as r_file:
+        return r_file.read()
